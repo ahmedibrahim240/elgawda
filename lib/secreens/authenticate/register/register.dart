b7fe42e405033302951441d8f271e66b107f3940
kahraba <Ahmedibrahim1310@icloud.com>
2021-05-05 01:04:21 +0200

    remoove import

--- lib/secreens/authenticate/register/register.dart
+++ lib/secreens/authenticate/register/register.dart
@@ -6,7 +6,6 @@ import 'package:elgawda/models/utils.dart';
 import 'package:elgawda/secreens/wrapper/wrapper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
